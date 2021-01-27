1028b083e0cc31ce6b0f4d62d5b6d3c2ef36259b
prasanthnag <kamma.prasanthnag@yahoo.com>
2021-01-27 13:55:33 +0530

    final commit

--- app/src/main/java/com/infy/newsmvvm/util/Constants.kt
+++ app/src/main/java/com/infy/newsmvvm/util/Constants.kt
@@ -1,8 +1,5 @@
 package com.infy.newsmvvm.util
 
 object Constants{
-    const val KEY_TITLE = "title"
-    const val KEY_DESCRIPTION = "description"
-    const val KEY_IMAGE_URL = "imageHref"
     const val BASE_URL = "https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/"
 }
